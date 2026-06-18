6840d685610e114dad61c81e067b19f816597f04
pdshah09 <paramshah2905@gmail.com>
2026-06-18 21:10:06 +0530

    bkpMosaic

--- src/components/UserMosaicSection.tsx
+++ src/components/UserMosaicSection.tsx
@@ -114,3 +114,142 @@ export default function UsersMosaicSection() {
     </section>
   );
 }
+
+/*
+<section class="border-none h-auto relative border-[rgb(55,65,81)] border-[3px]">
+  <div class="px-4 border-none mx-auto min-h-80 relative w-full border-[rgb(55,65,81)] border-[3px] md:max-w-[720px] lg:max-w-[1320px]">
+    <div class="items-center border-none flex justify-center absolute z-[1] top-[0px] left-[0px] right-[0px] bottom-[0px] border-[rgb(55,65,81)] border-[3px]">
+      <div class="bg-white border-none text-center text-[rgb(17,24,39)] border-[rgb(17,24,39)] p-16 border-[3px] md:rounded-[50rem] lg:py-16 lg:px-32 lg:p-0">
+        <h6 class="border-none font-bold relative mb-2 font-[Caveat] text-[rgb(17,24,39)] text-[calc(calc(1.6rem_+_4.2vw))] border-[rgb(17,24,39)] border-[3px] leading-none lg:text-[4.75rem]">
+          Join 15 million
+<span class="border-none bottom-auto hidden font-bold inset-x-auto absolute top-[-2.5rem] text-[1.5rem] text-[rgb(251,177,48)] border-[rgb(251,177,48)] border-[3px] md:block"> <img src="https://odoocdn.com/openerp_website/static/src/img/graphics/arrow_circular_down_left.svg" height="40px" class="border-none inline font-bold overflow-clip ml-[-8px] mr-2 mt-6 border-[rgb(251,177,48)] border-[3px]" loading="lazy" />
+          happy
+</span> users </h6>
+<span class="border-none inline text-[calc(calc(1.275rem_+_0.3vw))] border-[rgb(17,24,39)] border-[3px] lg:text-[1.5rem]">who grow their business with Odoo</span></div>
+    </div>
+    <div id="s_wd_users_mosaic" class="border-none flex flex-wrap justify-between border-[rgb(55,65,81)] border-[3px]">
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[80px_0px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[160px_0px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[240px_0px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[320px_0px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[400px_0px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[480px_0px] rounded-bl-[50%] rounded-br rounded-tl rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[560px_0px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[640px_0px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[720px_0px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[800px_0px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[880px_0px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[0px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[80px_80px] rounded-bl-[50%] rounded-br rounded-tl rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[160px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[240px_80px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[320px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[400px_80px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[480px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[560px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[640px_80px] rounded-bl-[50%] rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[720px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[800px_80px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[880px_80px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[0px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[80px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[160px_160px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[240px_160px] rounded-bl-[50%] rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[320px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[400px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[480px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[560px_160px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[640px_160px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[720px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[800px_160px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[880px_160px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[0px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[80px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[160px_240px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[240px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[320px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[400px_240px] rounded-bl-[50%] rounded-br-[50%] rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[480px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[560px_240px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[640px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[720px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[800px_240px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[880px_240px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[0px_320px] rounded-bl-[50%] rounded-br rounded-tl rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[80px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[160px_320px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[240px_320px] rounded-bl rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[320px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[400px_320px] rounded-bl rounded-br-[50%] rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[480px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_01.webp')] bg-position-[560px_320px] rounded-bl-[50%] rounded-br rounded-tl rounded-tr-[50%] w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[640px_320px] rounded-bl rounded-br rounded-tl-[50%] rounded-tr w-20 h-20 border-[rgb(55,65,81)] m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[720px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[800px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded m-3 border-[3px]">
+      </div>
+      <div class="border-none bg-[url('https://www.odoo.com/openerp_website/static/src/img/snippets/s_wd_users/users_02.webp')] bg-position-[880px_320px] w-20 h-20 border-[rgb(55,65,81)] rounded-[50%] m-3 border-[3px]">
+      </div>
+    </div>
+  </div>
+</section>
+
+*/
